7394521564f25d7a05edaa22f89426792d413e25
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-10-10 18:05:54 -0400

    feat: :sparkles: Created endpoint to fetch movies from external API

--- lib/endpoint.ex
+++ lib/endpoint.ex
@@ -15,6 +15,16 @@ defmodule MovieFinderMicroservicesElixir.Endpoint do
     send_resp(conn, 200, "pong!")
   end
 
+  get "/api/movies" do
+    movies = MovieFinder.Movies.list_movies_from_external_service(conn.query_params) |> IO.inspect
+
+    conn
+    |> send_resp(
+      200,
+      Poison.encode!(movies)
+    )
+  end
+
   match _ do
     send_resp(conn, 404, "Nothing here :(")
   end
