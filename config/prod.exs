fac814ee4aa0d984c9b7cf0b13b4255e8d96f6c6
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-10-11 00:52:23 -0400

    fix(docker): :fire: Followed format definition of env files for Docker

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :movie_finder, port: 8080
 
 config :movie_finder, :downstream_service,
-  movie_search_service_url: System.fetch_env!("MOVIE_SEARCH_SERVICE_URL"),
-  movie_info_service_url: System.fetch_env!("MOVIE_INFO_SERVICE_URL"),
-  artist_info_service_url: System.fetch_env!("ARTIST_INFO_SERVICE_URL")
+  movie_search_service_url: System.get_env("MOVIE_SEARCH_SERVICE_URL"),
+  movie_info_service_url: System.get_env("MOVIE_INFO_SERVICE_URL"),
+  artist_info_service_url: System.get_env("ARTIST_INFO_SERVICE_URL")
